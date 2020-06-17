074e672950886b352ff2aeec2abde3a34f8c9fa0
EC2 Default User <ec2-user@ip-172-31-41-126.eu-west-1.compute.internal>
2020-06-17 12:08:23 +0000

    third Change

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ Amplify.configure(config)
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!  hey Rohit</Text>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
